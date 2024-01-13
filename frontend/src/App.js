7a9bc013b6eac5118a441dfbd6d0e6bef06ce50c
Kyle P <powell-5@hotmail.com>
2024-01-13 14:45:32 -0500

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import "./css/form.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <div className="block">
+          <label>Email:</label>
+          <input />
+        </div>
+        <div className="block">
+          <label>Password:</label>
+          <input />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
