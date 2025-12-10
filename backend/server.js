80261f23488c47306a66ab54a8929bad4ef13460
Kyle P <powell-5@hotmail.com>
2025-12-10 01:27:43 -0500

    cors update

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/ChattyAp
 // middlewares
 app.use(express.json());
 app.use(cors({
-    origin: frontendArr,
+    origin: frontendUrl,
     allowedHeaders: ['Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     methods: "GET,POST,DELETE,OPTIONS",
     preflightContinue: false
