d0b01449f267ffc775adf9d857767a1673b93b35
Kyle P <powell-5@hotmail.com>
2025-12-11 12:19:55 -0500

    more live debug

--- backend/middleware/authCheck.js
+++ backend/middleware/authCheck.js
@@ -7,8 +7,7 @@ module.exports = (req, res, next) => {
         if (req.headers.authorization) {
             token = req.headers.authorization.split(' ')[1];
             console.log('auth', req.headers.authorization);
-            console.log('cookies', req.cookies);
-            if (!token) token = req.cookies.token;
+            console.log('token', token);
         } else {
             console.log('no header: ' + req.headers.authorization);
             throw new Error('No Header/Token')
