3e7c34da48a867f90905d1ade5874641abbbc791
Kyle P <powell-5@hotmail.com>
2025-12-11 13:08:27 -0500

    credentials added to socket frontend

--- frontend/src/util/socket.js
+++ frontend/src/util/socket.js
@@ -2,4 +2,9 @@ import { io } from "socket.io-client";
 
 const serverURL = process.env.REACT_APP_IO_URL;
 
-export const socket = io(serverURL, { autoConnect: false });
+export const socket = io(serverURL,
+    {
+        autoConnect: false,
+        withCredentials: true,
+        transports: ["websocket", "polling"]
+    });
